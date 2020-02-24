8944d6ee85650f1fb5ea04b9e688831d7855d361
Akhil Potla <akhil4400@gmail.com>
2020-02-24 13:53:58 -0600

    adding gaussian noise to prodcution data

--- src/data/process_dataset.py
+++ src/data/process_dataset.py
@@ -1,4 +1,13 @@
+import numpy as np
 import pandas as pd
+
+def gaussian_white_noise(column):
+    length = len(column)
+    mu, sigma = [column.mean(), column.std()]
+    noise = np.random.normal(loc=mu, scale=sigma, size=length)
+    for i in range(length):
+        column[i] += noise[i]
+    return column
 
 def net_flow(production):
     net = []
@@ -15,9 +24,13 @@ output_filename = "~/ut/research/crm_validation/data/interim/CRMP_Corrected_July
 df = pd.read_excel(input_filename, 0, skiprows=9)
 df = df.drop(df.columns[[3, 4, 7, 8]], axis=1)
 df.columns = [
-        'Time', 'Random_inj1', 'Random_inj2', 'Fixed_inj1', 'Fixed_inj2',
-        'Prod1', 'Prod2', 'Prod3', 'Prod4'
-    ]
+    'Time', 'Random_inj1', 'Random_inj2', 'Fixed_inj1', 'Fixed_inj2',
+    'Prod1', 'Prod2', 'Prod3', 'Prod4'
+]
+columns = df.columns[3:]
+for column in columns:
+    df[column] = gaussian_white_noise(df[column])
+
 df.insert(4, 'Net_Fixed_inj1', net_flow(df['Fixed_inj1']))
 df.insert(6, 'Net_Fixed_inj2', net_flow(df['Fixed_inj2']))
 df.insert(8, 'Net_Prod1', net_flow(df['Prod1']))
