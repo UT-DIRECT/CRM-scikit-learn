d617a043f1c9674c5a6a70fcdb3742559c8bb978
Akhil Potla <akhil4400@gmail.com>
2020-04-09 12:08:28 -0500

    r2 must have at least 2 points

--- src/helpers/analysis.py
+++ src/helpers/analysis.py
@@ -1,6 +1,11 @@
+import numpy as np
+
 from sklearn.metrics  import r2_score, mean_squared_error
 
 def fit_statistics(y_hat, y):
-    r2 = r2_score(y_hat, y)
+    if len(y_hat)  < 2:
+        r2 = np.nan
+    else:
+        r2 = r2_score(y_hat, y)
     mse = mean_squared_error(y_hat, y)
-    return [r2, mse]
+    return (r2, mse)
