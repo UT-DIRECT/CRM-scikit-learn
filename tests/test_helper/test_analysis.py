e11fd667be872f98f5ad6d3c0c4db2004c224fac
Akhil Potla <akhil4400@gmail.com>
2020-04-30 14:07:40 -0500

    Testing fit_statistics

--- tests/test_helper/test_analysis.py
+++ tests/test_helper/test_analysis.py
@@ -3,8 +3,19 @@ import numpy as np
 from src.helpers.analysis import fit_statistics
 
 
-def test_fit_statistics_y_hat_too_short():
-    y_hat = [1]
-    y = [1]
-    r2, mse = fit_statistics(y_hat, y)
-    assert(r2 in [np.nan])
+class TestFitStatistics():
+
+
+    def test_not_enough_data(self):
+        y_hat = [1]
+        y = [1]
+        r2, mse = fit_statistics(y_hat, y)
+        assert(r2 in [np.nan])
+
+
+    def test_fit_statistics(self):
+        y_hat = [1, 2, 3]
+        y = [1, 3, 4]
+        stats = fit_statistics(y_hat, y)
+        assert(len(stats) == 2)
+        assert(None not in stats)
