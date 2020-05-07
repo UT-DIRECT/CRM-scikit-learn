89a7ad65a6f20679e50208776262f7a30fba6e85
Akhil Potla <akhil4400@gmail.com>
2020-05-07 10:42:51 -0500

    Prediction tests

--- tests/test_models/test_crm.py
+++ tests/test_models/test_crm.py
@@ -1,12 +1,15 @@
 from unittest.mock import patch
 
 import numpy as np
-from sklearn.utils.validation import check_is_fitted
+import pytest
+from sklearn.utils.validation import check_is_fitted, NotFittedError
 
 from src.helpers.features import production_rate_dataset
 from src.models.crm import CRM
 
 
+# Initializing variables outside of the class since pytest ignores classes
+# with a constructor.
 q = np.array([2, 3, 4, 5, 6])
 inj1 = 2 * q
 inj2 = 3 * q
@@ -45,3 +48,41 @@ class TestCRM():
         assert(0 <= f3 <= 1)
         sum_of_gains = f1 + f2 + f3
         assert(abs(1 - sum_of_gains) <= 1.e-5)
+
+
+    def test_fit_X_y_different_shape(self):
+        X, y = production_rate_dataset(q, inj1, inj2, inj3)
+        X = X.T
+        X = X[:-2]
+        X = X.T
+        with pytest.raises(ValueError):
+            crm = CRM().fit(X, y)
+
+
+    def test_predict_unfitted_crm_raises_error(self):
+        X, y = production_rate_dataset(q, inj1, inj2, inj3)
+        crm = CRM()
+        with pytest.raises(NotFittedError):
+            crm.predict(X)
+
+
+    def test_predict_two_injectors(self):
+        X, y = production_rate_dataset(q, inj1, inj2)
+        crm = CRM().fit(X, y)
+        # There is no helper to construct the prediction matrix
+        # since the prediction matrix is constructed by the cross validator
+        X = [q[1:], inj1[1:], inj2[1:]]
+        y_hat = crm.predict(X)
+        assert(y_hat is not None)
+        assert(len(y_hat) == 4)
+
+
+    def test_predict_three_injectors(self):
+        X, y = production_rate_dataset(q, inj1, inj2, inj3)
+        crm = CRM().fit(X, y)
+        # There is no helper to construct the prediction matrix
+        # since the prediction matrix is constructed by the cross validator
+        X = [q[1:], inj1[1:], inj2[1:], inj3[1:]]
+        y_hat = crm.predict(X)
+        assert(y_hat is not None)
+        assert(len(y_hat) == 4)
