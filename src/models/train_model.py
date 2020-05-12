14e11f49cd0dfab9d3e1d2c573b1dcac5e1c381f
Akhil Potla <akhil4400@gmail.com>
2020-05-12 10:52:43 -0500

    Refactor training

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -13,8 +13,8 @@ from src.helpers.analysis import fit_statistics
 from src.helpers.cross_validation import (forward_walk_splitter,
         train_model_with_cv)
 from src.helpers.features import production_rate_dataset
-from src.helpers.models import serialized_model_file
-from src.models import producers, Fixed_inj1, Fixed_inj2, producer_names
+from src.helpers.models import serialized_model_path
+from src.models import injectors, producers, producer_names
 from src.models.crm import CRM
 
 
@@ -31,7 +31,7 @@ for i in range(len(producers)):
     models = [
         BayesianRidge(), CRM(), ElasticNetCV, LassoCV, LinearRegression()
     ]
-    X, y = production_rate_dataset(producers[i], Fixed_inj1, Fixed_inj2)
+    X, y = production_rate_dataset(producers[i], *injectors)
     train_split, test_split, train_test_seperation_idx = forward_walk_splitter(X, y, 2)
     X_train = X[:train_test_seperation_idx]
     y_train = y[:train_test_seperation_idx]
@@ -40,6 +40,6 @@ for i in range(len(producers)):
         if is_CV_model(model):
             model = train_model_with_cv(X, y, model, train_split)
         model = model.fit(X_train, y_train)
-        pickled_model = serialized_model_file(producer_names[i], model)
+        pickled_model = serialized_model_path(producer_names[i], model)
         with open(pickled_model, 'wb') as f:
             pickle.dump(model, f)
