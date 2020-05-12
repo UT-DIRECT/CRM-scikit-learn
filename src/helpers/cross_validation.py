cecdc682267e9a81a2378b1b9805c77a15d8443d
Akhil Potla <akhil4400@gmail.com>
2020-05-12 10:51:13 -0500

    Model helpers

--- src/helpers/cross_validation.py
+++ src/helpers/cross_validation.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import (BayesianRidge, ElasticNet, ElasticNetCV,
         Lasso, LassoCV, LinearRegression)
 
 from .analysis import fit_statistics
+from .models import test_model
 
 
 TRAINING_SPLIT = 0.8
@@ -22,9 +23,10 @@ def forward_walk_splitter(X, y, step_size):
         test_start = train_end + 1
         # test_start + step_size is one too long, so subtract by 1.
         # This is because linspace is inclusive.
-        test_end = (int(test_start + step_size - 1)
-            if test_start + step_size < length
-            else length - 1)
+        if test_start + step_size < length:
+            test_end = int(test_start + step_size - 1)
+        else:
+            break
         train = np.linspace(0, train_end, num=(train_end + 1)).astype(int)
         # Sometimes the testing set is smaller than the step_size.
         # This is to ensure that there are the same number of splits for
@@ -43,23 +45,11 @@ def train_and_test_model(X, y, model, train_test_splits):
     train_split = train_test_splits[0]
     test_split = train_test_splits[1]
     train_test_seperation_idx = train_test_splits[2]
-    r2_sum, mse_sum = 0, 0
-    length = len(test_split)
     if isinstance(model, LinearRegression) or isinstance(model, BayesianRidge):
         model.fit(X[:train_test_seperation_idx], y[:train_test_seperation_idx])
     else:
         model = train_model_with_cv(X, y, model, train_split)
-    for train, test in test_split:
-        x_train, x_test = X[train], X[test]
-        y_train, y_test = y[train], y[test]
-        model.fit(x_train, y_train)
-        y_predict = model.predict(x_test)
-        r2_i, mse_i = fit_statistics(y_predict, y_test)
-        r2_sum += r2_i
-        mse_sum += mse_i
-    r2 = r2_sum / length
-    mse = mse_sum / length
-    return (r2, mse)
+    return test_model(X, y, model, test_split)
 
 
 def train_model_with_cv(X, y, model, train_split):
