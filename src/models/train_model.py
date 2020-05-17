d6983055457cb42d1ee7869428f4abb3d2fd8e95
Akhil Potla <akhil4400@gmail.com>
2020-05-17 15:16:16 -0500

    Training net production models

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -12,21 +12,17 @@ from sklearn.linear_model import (BayesianRidge, ElasticNetCV, LassoCV,
 from src.helpers.analysis import fit_statistics
 from src.helpers.cross_validation import (forward_walk_splitter,
         train_model_with_cv)
-from src.helpers.features import production_rate_dataset
+from src.helpers.features import net_production_dataset, production_rate_dataset
 from src.helpers.models import serialized_model_path
-from src.models import injectors, producers, producer_names
+from src.models import injectors, net_productions, producers, producer_names
 from src.models.crm import CRM
-
-
-# TODO: make a CRM net production version
-def N2(self, N1, q2):
-    return N1 + q2
 
 
 def is_CV_model(model):
     return not (isinstance(model, LinearRegression) or isinstance(model, BayesianRidge) or isinstance(model, CRM))
 
 
+# Production Rate Training
 for i in range(len(producers)):
     models = [
         BayesianRidge(), CRM(), ElasticNetCV, LassoCV, LinearRegression()
@@ -41,5 +37,27 @@ for i in range(len(producers)):
             model = train_model_with_cv(X, y, model, train_split)
         model = model.fit(X_train, y_train)
         pickled_model = serialized_model_path(producer_names[i], model)
+        with open(pickled_model, 'wb') as f:
+            pickle.dump(model, f)
+
+
+# Net Production Training
+for i in range(len(producers)):
+    models = [
+        BayesianRidge(), CRM(), ElasticNetCV, LassoCV, LinearRegression()
+    ]
+    X, y = net_production_dataset(net_productions[i], producers[i], *injectors)
+    train_split, test_split, train_test_seperation_idx = forward_walk_splitter(X, y, 2)
+    X_train = X[:train_test_seperation_idx]
+    y_train = y[:train_test_seperation_idx]
+
+    for model in models:
+        if is_CV_model(model):
+            model = train_model_with_cv(X, y, model, train_split)
+        model = model.fit(X_train, y_train)
+        pickled_model = serialized_model_path(
+            'Net {}'.format(producer_names[i]),
+            model
+        )
         with open(pickled_model, 'wb') as f:
             pickle.dump(model, f)
