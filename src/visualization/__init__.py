36c297b50c670dbe176b18a4eded3e30dffb0018
Akhil Potla <akhil4400@gmail.com>
2020-05-19 09:04:29 -0500

    Net production plots

--- src/visualization/__init__.py
+++ src/visualization/__init__.py
@@ -1,3 +1,4 @@
 from src.config import INPUTS
 
 q_predictions_metrics_file = INPUTS['files']['q_predictions_metrics']
+N_predictions_metrics_file = INPUTS['files']['N_predictions_metrics']
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,7 +5,8 @@ import pandas as pd
 from src.models import (injectors, net_productions,
     producers, producer_names, step_sizes, Time)
 from src.helpers.figures import bar_plot_helper, bar_plot_formater, plot_helper
-from src.visualization import q_predictions_metrics_file
+from src.visualization import (N_predictions_metrics_file,
+    q_predictions_metrics_file)
 
 
 def producers_vs_time():
@@ -46,7 +47,7 @@ def producers_vs_injector():
         )
 
 
-def net_production_estimators_and_time_steps():
+def production_rate_estimators_and_time_steps():
     x_labels = [int(step_size) for step_size in step_sizes]
     predictions_metrics_df = pd.read_csv(q_predictions_metrics_file)
     x = np.arange(len(x_labels))
@@ -81,7 +82,7 @@ def net_production_estimators_and_time_steps():
         bar_plot_formater(x, x_labels, title, xlabel, ylabel)
 
 
-def net_production_good_estimators_and_time_steps():
+def production_rate_good_estimators_and_time_steps():
     x_labels = [int(step_size) for step_size in step_sizes]
     predictions_metrics_df = pd.read_csv(q_predictions_metrics_file)
     x = np.arange(len(x_labels))
@@ -112,6 +113,72 @@ def net_production_good_estimators_and_time_steps():
         bar_plot_formater(x, x_labels, title, xlabel, ylabel)
 
 
+def net_production_estimators_and_time_steps():
+    x_labels = [int(step_size) for step_size in step_sizes]
+    predictions_metrics_df = pd.read_csv(N_predictions_metrics_file)
+    x = np.arange(len(x_labels))
+    width = 0.15
+    bar_labels = [
+        'CRM, mse', 'Linear Regression, mse', 'Bayesian Ridge, mse',
+        'Lasso, mse', 'Elastic, mse'
+    ]
+    for i in range(len(producers)):
+        producer = i + 1
+        producer_rows_df = predictions_metrics_df.loc[predictions_metrics_df['Producer'] == producer]
+        crm_mse = []
+        linear_regression_mse = []
+        bayesian_ridge_mse = []
+        lasso_mse = []
+        elastic_mse = []
+        heights = [
+            crm_mse, linear_regression_mse, bayesian_ridge_mse, lasso_mse,
+            elastic_mse
+        ]
+        models = ['CRM', 'LinearRegression', 'BayesianRidge', 'Lasso', 'ElasticNet']
+        for i in range(len(models)):
+            mses = producer_rows_df.loc[producer_rows_df['Model'] == models[i]]
+            for step_size in step_sizes:
+                mse = mses.loc[mses['Step size'] == step_size]['MSE']
+                heights[i].append(float(mse))
+
+        title = 'Net Production Estimators Producer {}'.format(producer)
+        xlabel = 'Step Size'
+        ylabel = 'Mean Squared Error'
+        bar_plot_helper(width, x, x_labels, bar_labels, heights)
+        bar_plot_formater(x, x_labels, title, xlabel, ylabel)
+
+
+def net_production_good_estimators_and_time_steps():
+    x_labels = [int(step_size) for step_size in step_sizes]
+    predictions_metrics_df = pd.read_csv(N_predictions_metrics_file)
+    x = np.arange(len(x_labels))
+    width = 0.23
+    bar_labels = [
+        'CRM, mse', 'Linear Regression, mse', 'Bayesian Ridge, mse'
+    ]
+    for i in range(len(producers)):
+        producer = i + 1
+        producer_rows_df = predictions_metrics_df.loc[predictions_metrics_df['Producer'] == producer]
+        crm_mse = []
+        linear_regression_mse = []
+        bayesian_ridge_mse = []
+        heights = [
+            crm_mse, linear_regression_mse, bayesian_ridge_mse
+        ]
+        models = ['CRM', 'LinearRegression', 'BayesianRidge']
+        for i in range(len(models)):
+            mses = producer_rows_df.loc[producer_rows_df['Model'] == models[i]]
+            for step_size in step_sizes:
+                mse = mses.loc[mses['Step size'] == step_size]['MSE']
+                heights[i].append(float(mse))
+
+        title = 'Net Production Estimators Good Estimator MSEs Producer {}'.format(producer)
+        xlabel = 'Step Size'
+        ylabel = 'Mean Squared Error'
+        bar_plot_helper(width, x, x_labels, bar_labels, heights)
+        bar_plot_formater(x, x_labels, title, xlabel, ylabel)
+
+
 def animated_net_production_predictions():
     pass
 
@@ -119,5 +186,7 @@ def animated_net_production_predictions():
 producers_vs_time()
 net_production_vs_time()
 producers_vs_injector()
+production_rate_estimators_and_time_steps()
+production_rate_good_estimators_and_time_steps()
 net_production_estimators_and_time_steps()
 net_production_good_estimators_and_time_steps()
