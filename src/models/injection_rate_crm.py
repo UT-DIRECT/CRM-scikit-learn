eb0af064d9ecb8b6e286a7aca788a31c19e5b241
Akhil Potla <akhil4400@gmail.com>
2020-06-01 15:30:11 -0500

    Refactoring

--- src/models/injection_rate_crm.py
+++ src/models/injection_rate_crm.py
@@ -11,7 +11,6 @@ class InjectionRateCRM(CRM):
 
 
     def predict(self, X):
-        # TODO: This is not done, this needs to optimize the injection rate
         check_is_fitted(self)
         X = X.T
         self.X_predict_ = X
@@ -21,8 +20,8 @@ class InjectionRateCRM(CRM):
 
     def _objective_function(self, params):
         injection_rates = params
-        n_gains = len(self.X_predict_) - 1
-        params = np.reshape(params, (n_gains, -1)).tolist()
+        self.n_gains = len(self.X_predict_) - 1
+        params = np.reshape(params, (self.n_gains, -1)).tolist()
         X = params
         X.insert(0, self.X_predict_[0].tolist())
         X = np.array(X)
@@ -31,12 +30,12 @@ class InjectionRateCRM(CRM):
 
     def _fit_injection_rate(self, X):
         # The CRM function is part of the _sum_residuals function
-        n_gains = len(X) - 1
+        self.n_gains = len(X) - 1
         time_steps = len(X[0])
-        lower_bounds = np.ones(n_gains * time_steps) * np.min(X[1:]) * 0.8
-        upper_bounds = np.ones(n_gains * time_steps) * np.max(X[1:]) * 1.2
+        lower_bounds = np.ones(self.n_gains * time_steps) * np.min(X[1:]) * 0.8
+        upper_bounds = np.ones(self.n_gains * time_steps) * np.max(X[1:]) * 1.2
         bounds = np.array([lower_bounds, upper_bounds]).T.tolist()
-        p0 = np.ones(n_gains * time_steps) * np.average(X[1:])
+        p0 = np.ones(self.n_gains * time_steps) * np.average(X[1:])
         params = fmin_slsqp(
             self._objective_function, p0, f_eqcons=self._constraints,
             bounds=bounds, iter=1000, iprint=0
