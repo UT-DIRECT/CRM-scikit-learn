0acff241ab75dfa55838e2566f7294e6649c4797
Akhil Potla <akhil4400@gmail.com>
2020-06-01 10:54:18 -0500

    Fixed imports

--- src/models/injection_rate_crm.py
+++ src/models/injection_rate_crm.py
@@ -2,8 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from scipy.optimize import fmin_slsqp
-from sklearn.base import BaseEstimator, RegressorMixin
-from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.validation import check_is_fitted
 
 from src.models.crm import CRM
 
