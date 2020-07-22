e7a1adcd0c77161a36d0dbb695d2626e174c7d27
Akhil Potla <akhil4400@gmail.com>
2020-07-22 11:12:17 -0500

    CRMT skeleton

---
+++ src/models/crmt.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from scipy.optimize import fmin_slsqp
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils.validation import check_X_y, check_is_fitted
+
+
+class CRMT(BaseEstimator, RegressorMixin):
+
+
+    def __init__(self):
+        return
+
+
+    def fit(self, X=None, y=None):
+        pass
+
+
+    def predict(self, X):
+        pass
