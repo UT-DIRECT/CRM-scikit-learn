3d834bcf3ab97dc550985c419fd00702af220c09
Akhil Potla <akhil4400@gmail.com>
2020-07-24 09:26:24 -0500

    Calculate delta time and read it in

--- src/data/read_wfsim.py
+++ src/data/read_wfsim.py
@@ -5,9 +5,17 @@ from src.config import INPUTS
 
 
 def read_wfsim(data_file):
-    pass
+    data = np.loadtxt(data_file, delimiter=',', skiprows=1).T
+    time = data[0]
+    delta_time = data[1]
+    qo_tank = data[2]
+    w_tank = data[3]
+    qw_tank = data[4]
+    q_tank = data[5]
+    features = [time, delta_time, qo_tank, w_tank, qw_tank, q_tank]
+    return features
 
 
 data_file = INPUTS['wfsim']['data']
 features = read_wfsim(data_file)
-[Time, qo_tank, w_tank, qw_tank, q_tank] = features
+[time, delta_time, qo_tank, w_tank, qw_tank, q_tank] = features
--- src/features/build_wfsim_features.py
+++ src/features/build_wfsim_features.py
@@ -3,6 +3,12 @@ import pandas as pd
 from src.config import INPUTS
 
 
+def delta_time_calculator(time):
+    delta_time = [0]
+    for i in range(1, len(time)):
+        delta_time.append(time[i] - time[i -  1])
+    return delta_time
+
 raw_data_file = INPUTS['wfsim']['raw_data']
 processed_data_file = INPUTS['wfsim']['data']
 
@@ -11,5 +17,7 @@ df = pd.read_excel(raw_data_file, sheet_name=0, skiprows=2)
 df = df.drop(df.columns[[0, 3, 5, 7, 9, 10, 11, 12]], axis=1)
 df = df.rename(columns={'Unnamed: 1': 'Time'}, errors='raise')
 df = df.dropna()
+
+df.insert(1, "Delta_Time", delta_time_calculator(df['Time']))
 
 df.to_csv(processed_data_file, index=False)
