db0384929e64003ee5c83098124ca0f7632a86f9
Akhil Potla <akhil4400@gmail.com>
2020-10-28 09:44:46 -0500

    Sensitivity plot is now 2D.

--- src/visualization/crmp_sensitivity_analysis.py
+++ src/visualization/crmp_sensitivity_analysis.py
@@ -13,49 +13,47 @@ q_sensitivity_analysis_file = INPUTS['crmp']['q_sensitivity_analysis']
 FIG_DIR = INPUTS['crmp']['figures_dir']
 
 true_parameters = {
-    1: [0.2, 0.8, 1.5],
-    2: [0.4, 0.6, 1],
-    3: [0.6, 0.4, 5],
-    4: [0.8, 0.2, 50]
+    1: [0.2, 1.5],
+    2: [0.4, 1],
+    3: [0.6, 5],
+    4: [0.8, 50]
 }
 
 
 def plot_parameter_convergence():
     q_sensitivity_analysis_df = pd.read_csv(q_sensitivity_analysis_file)
     for i in range(len(producers)):
-        fig =  plt.figure()
-        ax = Axes3D(fig)
         producer = i + 1
         producer_rows_df = q_sensitivity_analysis_df.loc[q_sensitivity_analysis_df['Producer'] == producer]
         x_i = producer_rows_df['f1_initial']
         x_f = producer_rows_df['f1_final']
-        y_i = producer_rows_df['f2_initial']
-        y_f = producer_rows_df['f2_final']
-        z_i = producer_rows_df['tau_initial']
-        z_f = producer_rows_df['tau_final']
+        y_i = producer_rows_df['tau_initial']
+        y_f = producer_rows_df['tau_final']
         x = np.array([x_i, x_f]).T
         y = np.array([y_i, y_f]).T
-        z = np.array([z_i, z_f]).T
         true_params = true_parameters[producer]
         x_true = true_params[0]
         y_true = true_params[1]
-        z_true = true_params[2]
-        initial = ax.scatter(x_i, y_i, z_i, s=40, c='g', marker='o', label='Initial')
-        final = ax.scatter(x_f, y_f, z_f, s=40, c='r', marker='x', label='Final')
+        initial = plt.scatter(x_i, y_i, s=40, c='g', marker='o', label='Initial')
+        final = plt.scatter(x_f, y_f, s=40, c='r', marker='x', label='Final')
         for i in range(len(x)):
-            ax.plot(x[i], y[i], z[i], c='k', alpha=0.3)
-        actual = ax.scatter(
-            x_true, y_true, z_true, s=200, c='b', marker='X',
+            plt.plot(x[i], y[i], c='k', alpha=0.3)
+        actual = plt.scatter(
+            x_true, y_true, s=200, c='b', marker='X',
             label='Actual'
         )
-        ax.set_title(
+        plt.title(
             'Producer {}: Initial Parameter Values with Convergence'.format(producer),
             fontsize=14,
         )
-        ax.set_xlabel('f1', fontsize=12)
-        ax.set_ylabel('f2', fontsize=12)
-        ax.set_zlabel('tau', fontsize=12)
-        ax.legend(handles=[actual, initial, final], loc="lower left")
+        plt.xlabel('f1', fontsize=12)
+        plt.ylabel('tau', fontsize=12)
+        plt.legend(
+            handles=[actual, initial, final],
+            bbox_to_anchor=(1.04, 1),
+            loc="upper left"
+        )
+        plt.tight_layout()
         filename = 'producer_{}_initial_parameter_values_with_convergence'.format(producer)
         fig_file = '{}/{}'.format(FIG_DIR, filename)
         plt.savefig(fig_file)
