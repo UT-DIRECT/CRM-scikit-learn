76ce318963d9e6cf839b97358f0bd62248585877
Akhil Potla <akhil4400@gmail.com>
2020-11-29 10:02:31 -0600

    Fitting contour plots

--- src/visualization/crmp_sensitivity_analysis.py
+++ src/visualization/crmp_sensitivity_analysis.py
@@ -7,7 +7,8 @@ from src.helpers.figures import plot_helper
 from src.visualization import INPUTS
 
 
-q_sensitivity_analysis_file = INPUTS['crmp']['q_sensitivity_analysis']
+q_fitting_sensitivity_analysis_file = INPUTS['crmp']['q_fitting_sensitivity_analysis']
+q_predictions_sensitivity_analysis_file = INPUTS['crmp']['q_predictions_sensitivity_analysis']
 FIG_DIR = INPUTS['crmp']['figures_dir']
 
 true_parameters = {
@@ -18,12 +19,12 @@ true_parameters = {
 }
 
 
-def plot_parameter_convergence():
-    q_sensitivity_analysis_df = pd.read_csv(q_sensitivity_analysis_file)
+def plot_parameter_convergence_fitting():
+    q_fitting_sensitivity_analysis_df = pd.read_csv(q_fitting_sensitivity_analysis_file)
     for i in range(len(producers)):
         plt.figure(figsize=[7, 4.8])
         producer = i + 1
-        producer_rows_df = q_sensitivity_analysis_df.loc[q_sensitivity_analysis_df['Producer'] == producer]
+        producer_rows_df = q_fitting_sensitivity_analysis_df.loc[q_fitting_sensitivity_analysis_df['Producer'] == producer]
         x_i = producer_rows_df['f1_initial']
         x_f = producer_rows_df['f1_final']
         y_i = producer_rows_df['tau_initial']
@@ -41,7 +42,7 @@ def plot_parameter_convergence():
             x_true, y_true, s=200, c='b', marker='X',
             label='Actual'
         )
-        title = 'CRMP Producer {}: Initial Parameter Values with Convergence'.format(producer)
+        title = 'CRMP Fitting: Producer {} Initial Parameter Values with Convergence'.format(producer)
         xlabel ='f1'
         ylabel ='tau'
         plt.legend(
@@ -59,11 +60,52 @@ def plot_parameter_convergence():
         )
 
 
-def initial_guesses_and_mean_squared_error():
-    q_sensitivity_analysis_df = pd.read_csv(q_sensitivity_analysis_file)
+def plot_parameter_convergence_prediction():
+    q_predictions_sensitivity_analysis_df = pd.read_csv(q_predictions_sensitivity_analysis_file)
+    for i in range(len(producers)):
+        plt.figure(figsize=[7, 4.8])
+        producer = i + 1
+        producer_rows_df = q_predictions_sensitivity_analysis_df.loc[q_predictions_sensitivity_analysis_df['Producer'] == producer]
+        x_i = producer_rows_df['f1_initial']
+        x_f = producer_rows_df['f1_final']
+        y_i = producer_rows_df['tau_initial']
+        y_f = producer_rows_df['tau_final']
+        x = np.array([x_i, x_f]).T
+        y = np.array([y_i, y_f]).T
+        true_params = true_parameters[producer]
+        x_true = true_params[0]
+        y_true = true_params[1]
+        initial = plt.scatter(x_i, y_i, s=40, c='g', marker='o', label='Initial')
+        final = plt.scatter(x_f, y_f, s=40, c='r', marker='x', label='Final')
+        for i in range(len(x)):
+            plt.plot(x[i], y[i], c='k', alpha=0.3)
+        actual = plt.scatter(
+            x_true, y_true, s=200, c='b', marker='X',
+            label='Actual'
+        )
+        title = 'CRMP Prediction: Producer {} Initial Parameter Values with Convergence'.format(producer)
+        xlabel ='f1'
+        ylabel ='tau'
+        plt.legend(
+            handles=[actual, initial, final],
+            bbox_to_anchor=(1.04, 1),
+            loc="upper left"
+        )
+        plt.tight_layout()
+        plot_helper(
+            FIG_DIR,
+            title=title,
+            xlabel=xlabel,
+            ylabel=ylabel,
+            save=True
+        )
+
+
+def initial_guesses_and_mean_squared_error_fitting():
+    q_fitting_sensitivity_analysis_df = pd.read_csv(q_fitting_sensitivity_analysis_file)
     for i in range(len(producers)):
         producer = i + 1
-        producer_rows_df = q_sensitivity_analysis_df.loc[q_sensitivity_analysis_df['Producer'] == producer]
+        producer_rows_df = q_fitting_sensitivity_analysis_df.loc[q_fitting_sensitivity_analysis_df['Producer'] == producer]
         x = producer_rows_df['f1_initial'].to_numpy()
         x = np.reshape(x, (101, 11))
         y = producer_rows_df['tau_initial'].to_numpy()
@@ -76,7 +118,7 @@ def initial_guesses_and_mean_squared_error():
         x, y = true_parameters[producer]
         actual = plt.scatter(x, y, c='red', label='Actual')
         plt.legend(handles=[actual])
-        title = 'CRMP: Producer {} Intial Guesses with ln(MSE)'.format(producer)
+        title = 'CRMP Fitting: Producer {} Intial Guesses with ln(MSE)'.format(producer)
         xlabel = 'f1'
         ylabel = 'Tau'
         plot_helper(
@@ -88,5 +130,36 @@ def initial_guesses_and_mean_squared_error():
         )
 
 
-# plot_parameter_convergence()
-initial_guesses_and_mean_squared_error()
+def initial_guesses_and_mean_squared_error_prediction():
+    q_predictions_sensitivity_analysis_df = pd.read_csv(q_predictions_sensitivity_analysis_file)
+    for i in range(len(producers)):
+        producer = i + 1
+        producer_rows_df = q_predictions_sensitivity_analysis_df.loc[q_predictions_sensitivity_analysis_df['Producer'] == producer]
+        x = producer_rows_df['f1_initial'].to_numpy()
+        x = np.reshape(x, (101, 11))
+        y = producer_rows_df['tau_initial'].to_numpy()
+        y = np.reshape(y, (101, 11))
+        z = producer_rows_df['MSE'].to_numpy()
+        z = np.log(z)
+        z = np.reshape(z, (101, 11))
+        plt.contourf(x, y, z)
+        plt.colorbar()
+        x, y = true_parameters[producer]
+        actual = plt.scatter(x, y, c='red', label='Actual')
+        plt.legend(handles=[actual])
+        title = 'CRMP Prediction: Producer {} Intial Guesses with ln(MSE)'.format(producer)
+        xlabel = 'f1'
+        ylabel = 'Tau'
+        plot_helper(
+            FIG_DIR,
+            title=title,
+            xlabel=xlabel,
+            ylabel=ylabel,
+            save=True
+        )
+
+
+plot_parameter_convergence_fitting()
+plot_parameter_convergence_prediction()
+initial_guesses_and_mean_squared_error_fitting()
+initial_guesses_and_mean_squared_error_prediction()
