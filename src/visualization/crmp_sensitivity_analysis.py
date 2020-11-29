b073fb1d46be1da04fe57ec6832e2c05d51e4750
Akhil Potla <akhil4400@gmail.com>
2020-11-29 10:48:47 -0600

    Extract functions from sensitivity analysis plots

--- src/visualization/crmp_sensitivity_analysis.py
+++ src/visualization/crmp_sensitivity_analysis.py
@@ -8,7 +8,11 @@ from src.visualization import INPUTS
 
 
 q_fitting_sensitivity_analysis_file = INPUTS['crmp']['q_fitting_sensitivity_analysis']
+q_fitting_sensitivity_analysis_df = pd.read_csv(q_fitting_sensitivity_analysis_file)
+
 q_predictions_sensitivity_analysis_file = INPUTS['crmp']['q_predictions_sensitivity_analysis']
+q_predictions_sensitivity_analysis_df = pd.read_csv(q_predictions_sensitivity_analysis_file)
+
 FIG_DIR = INPUTS['crmp']['figures_dir']
 
 true_parameters = {
@@ -19,23 +23,45 @@ true_parameters = {
 }
 
 
+def _producer_rows_from_df(df, producer):
+    return df.loc[df['Producer'] == producer]
+
+
+def _initial_and_final_parameters_from_df(df):
+    x_i = df['f1_initial']
+    x_f = df['f1_final']
+    y_i = df['tau_initial']
+    y_f = df['tau_final']
+    x = np.array([x_i, x_f]).T
+    y = np.array([y_i, y_f]).T
+    return (x, y)
+
+
+def _contour_parameters(df):
+    x = producer_rows_df['f1_initial'].to_numpy()
+    x = np.reshape(x, (101, 11))
+    y = producer_rows_df['tau_initial'].to_numpy()
+    y = np.reshape(y, (101, 11))
+    z = producer_rows_df['MSE'].to_numpy()
+    z = np.log(z)
+    z = np.reshape(z, (101, 11))
+    return (x, y, z)
+
+
 def plot_parameter_convergence_fitting():
-    q_fitting_sensitivity_analysis_df = pd.read_csv(q_fitting_sensitivity_analysis_file)
     for i in range(len(producers)):
         plt.figure(figsize=[7, 4.8])
         producer = i + 1
-        producer_rows_df = q_fitting_sensitivity_analysis_df.loc[q_fitting_sensitivity_analysis_df['Producer'] == producer]
-        x_i = producer_rows_df['f1_initial']
-        x_f = producer_rows_df['f1_final']
-        y_i = producer_rows_df['tau_initial']
-        y_f = producer_rows_df['tau_final']
-        x = np.array([x_i, x_f]).T
-        y = np.array([y_i, y_f]).T
+        producer_rows_df = _producer_rows_from_df(
+            q_fitting_sensitivity_analysis_df,
+            producer
+        )
+        x, y = _initial_and_final_parameters_from_df(producer_rows_df)
         true_params = true_parameters[producer]
         x_true = true_params[0]
         y_true = true_params[1]
-        initial = plt.scatter(x_i, y_i, s=40, c='g', marker='o', label='Initial')
-        final = plt.scatter(x_f, y_f, s=40, c='r', marker='x', label='Final')
+        initial = plt.scatter(x[0], y[0], s=40, c='g', marker='o', label='Initial')
+        final = plt.scatter(x[1], y[1], s=40, c='r', marker='x', label='Final')
         for i in range(len(x)):
             plt.plot(x[i], y[i], c='k', alpha=0.3)
         actual = plt.scatter(
@@ -61,22 +87,19 @@ def plot_parameter_convergence_fitting():
 
 
 def plot_parameter_convergence_prediction():
-    q_predictions_sensitivity_analysis_df = pd.read_csv(q_predictions_sensitivity_analysis_file)
     for i in range(len(producers)):
         plt.figure(figsize=[7, 4.8])
         producer = i + 1
-        producer_rows_df = q_predictions_sensitivity_analysis_df.loc[q_predictions_sensitivity_analysis_df['Producer'] == producer]
-        x_i = producer_rows_df['f1_initial']
-        x_f = producer_rows_df['f1_final']
-        y_i = producer_rows_df['tau_initial']
-        y_f = producer_rows_df['tau_final']
-        x = np.array([x_i, x_f]).T
-        y = np.array([y_i, y_f]).T
+        producer_rows_df = _producer_rows_from_df(
+            q_predictions_sensitivity_analysis_df,
+            producer
+        )
+        x, y = _initial_and_final_parameters_from_df(producer_rows_df)
         true_params = true_parameters[producer]
         x_true = true_params[0]
         y_true = true_params[1]
-        initial = plt.scatter(x_i, y_i, s=40, c='g', marker='o', label='Initial')
-        final = plt.scatter(x_f, y_f, s=40, c='r', marker='x', label='Final')
+        initial = plt.scatter(x[0], y[0], s=40, c='g', marker='o', label='Initial')
+        final = plt.scatter(x[1], y[1], s=40, c='r', marker='x', label='Final')
         for i in range(len(x)):
             plt.plot(x[i], y[i], c='k', alpha=0.3)
         actual = plt.scatter(
@@ -105,14 +128,11 @@ def initial_guesses_and_mean_squared_error_fitting():
     q_fitting_sensitivity_analysis_df = pd.read_csv(q_fitting_sensitivity_analysis_file)
     for i in range(len(producers)):
         producer = i + 1
-        producer_rows_df = q_fitting_sensitivity_analysis_df.loc[q_fitting_sensitivity_analysis_df['Producer'] == producer]
-        x = producer_rows_df['f1_initial'].to_numpy()
-        x = np.reshape(x, (101, 11))
-        y = producer_rows_df['tau_initial'].to_numpy()
-        y = np.reshape(y, (101, 11))
-        z = producer_rows_df['MSE'].to_numpy()
-        z = np.log(z)
-        z = np.reshape(z, (101, 11))
+        producer_rows_df = _producer_rows_from_df(
+            q_fitting_sensitivity_analysis_df,
+            producer
+        )
+        x, y, z = _contour_parameters(producer_rows_df)
         plt.contourf(x, y, z)
         plt.colorbar()
         x, y = true_parameters[producer]
@@ -134,14 +154,11 @@ def initial_guesses_and_mean_squared_error_prediction():
     q_predictions_sensitivity_analysis_df = pd.read_csv(q_predictions_sensitivity_analysis_file)
     for i in range(len(producers)):
         producer = i + 1
-        producer_rows_df = q_predictions_sensitivity_analysis_df.loc[q_predictions_sensitivity_analysis_df['Producer'] == producer]
-        x = producer_rows_df['f1_initial'].to_numpy()
-        x = np.reshape(x, (101, 11))
-        y = producer_rows_df['tau_initial'].to_numpy()
-        y = np.reshape(y, (101, 11))
-        z = producer_rows_df['MSE'].to_numpy()
-        z = np.log(z)
-        z = np.reshape(z, (101, 11))
+        producer_rows_df = _producer_rows_from_df(
+            q_predictions_sensitivity_analysis_df,
+            producer
+        )
+        x, y, z = _contour_parameters(producer_rows_df)
         plt.contourf(x, y, z)
         plt.colorbar()
         x, y = true_parameters[producer]
