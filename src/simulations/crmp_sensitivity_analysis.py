f21bde3040f6960b8ba515936efb242ee7505994
Akhil Potla <akhil4400@gmail.com>
2020-11-29 10:02:15 -0600

    Fitting sensitivity analysis

--- src/simulations/crmp_sensitivity_analysis.py
+++ src/simulations/crmp_sensitivity_analysis.py
@@ -13,8 +13,14 @@ from src.helpers.models import model_namer, test_model
 from src.models.crmp import CRMP
 
 
-q_sensitivity_analysis_file = INPUTS['crmp']['q_sensitivity_analysis']
-q_sensitivity_analysis_data = {
+q_fitting_sensitivity_analysis_file = INPUTS['crmp']['q_fitting_sensitivity_analysis']
+q_predictions_sensitivity_analysis_file = INPUTS['crmp']['q_predictions_sensitivity_analysis']
+q_fitting_sensitivity_analysis_data = {
+    'Producer': [], 'Model': [], 'tau_initial': [], 'tau_final': [],
+    'f1_initial': [], 'f1_final': [], 'f2_initial': [], 'f2_final': [],
+    'r2': [], 'MSE': []
+}
+q_predictions_sensitivity_analysis_data = {
     'Producer': [], 'Model': [], 'tau_initial': [], 'tau_final': [],
     'f1_initial': [], 'f1_final': [], 'f2_initial': [], 'f2_final': [],
     'r2': [], 'MSE': []
@@ -38,35 +44,41 @@ for i in range(len(producers)):
     y_train = y[:train_test_seperation_idx]
     X_test = X[train_test_seperation_idx:]
     y_test = y[train_test_seperation_idx:]
-#     crmp_cv = GridSearchCV(CRMP(), param_grid,
-#         scoring=make_scorer(mean_squared_error, greater_is_better=False),
-#         cv=train_split
-#     )
-#     crmp_cv.fit(X, y)
-#     print(crmp_cv.get_params())
-#     raise
-#     print(crmp_cv.best_params_)
-#     print(crmp_cv.best_score_)
-#     print(1)
-#     print(crmp_cv.predict(X_test))
-#     print(cross_val_score(crmp_cv, X, y, cv=test_split))
-#     print(2)
     for p0 in param_grid['p0']:
         crmp = CRMP(p0=p0)
         crmp = crmp.fit(X_train, y_train)
-        r2, mse, y_hat, time_step = test_model(X, y, crmp, test_split)
-        # y_hat = crmp.predict(X_train)
-        # r2, mse = fit_statistics(y_train, y_hat)
-        q_sensitivity_analysis_data['Producer'].append(i + 1)
-        q_sensitivity_analysis_data['Model'].append(model_namer(crmp))
-        q_sensitivity_analysis_data['tau_initial'].append(p0[0])
-        q_sensitivity_analysis_data['tau_final'].append(crmp.tau_)
-        q_sensitivity_analysis_data['f1_initial'].append(p0[1])
-        q_sensitivity_analysis_data['f1_final'].append(crmp.gains_[0])
-        q_sensitivity_analysis_data['f2_initial'].append(p0[2])
-        q_sensitivity_analysis_data['f2_final'].append(crmp.gains_[1])
-        q_sensitivity_analysis_data['r2'].append(r2)
-        q_sensitivity_analysis_data['MSE'].append(mse)
 
-q_sensitivity_analysis_df = pd.DataFrame(q_sensitivity_analysis_data)
-q_sensitivity_analysis_df.to_csv(q_sensitivity_analysis_file)
+        # Fitting
+        y_hat = crmp.predict(X_train)
+        r2, mse = fit_statistics(y_hat, y_train)
+        q_fitting_sensitivity_analysis_data['Producer'].append(i + 1)
+        q_fitting_sensitivity_analysis_data['Model'].append(model_namer(crmp))
+        q_fitting_sensitivity_analysis_data['tau_initial'].append(p0[0])
+        q_fitting_sensitivity_analysis_data['tau_final'].append(crmp.tau_)
+        q_fitting_sensitivity_analysis_data['f1_initial'].append(p0[1])
+        q_fitting_sensitivity_analysis_data['f1_final'].append(crmp.gains_[0])
+        q_fitting_sensitivity_analysis_data['f2_initial'].append(p0[2])
+        q_fitting_sensitivity_analysis_data['f2_final'].append(crmp.gains_[1])
+        q_fitting_sensitivity_analysis_data['r2'].append(r2)
+        q_fitting_sensitivity_analysis_data['MSE'].append(mse)
+
+        # Prediction
+        r2, mse, y_hat, time_step = test_model(X, y, crmp, test_split)
+        q_predictions_sensitivity_analysis_data['Producer'].append(i + 1)
+        q_predictions_sensitivity_analysis_data['Model'].append(model_namer(crmp))
+        q_predictions_sensitivity_analysis_data['tau_initial'].append(p0[0])
+        q_predictions_sensitivity_analysis_data['tau_final'].append(crmp.tau_)
+        q_predictions_sensitivity_analysis_data['f1_initial'].append(p0[1])
+        q_predictions_sensitivity_analysis_data['f1_final'].append(crmp.gains_[0])
+        q_predictions_sensitivity_analysis_data['f2_initial'].append(p0[2])
+        q_predictions_sensitivity_analysis_data['f2_final'].append(crmp.gains_[1])
+        q_predictions_sensitivity_analysis_data['r2'].append(r2)
+        q_predictions_sensitivity_analysis_data['MSE'].append(mse)
+
+# Fitting
+q_fitting_sensitivity_analysis_df = pd.DataFrame(q_fitting_sensitivity_analysis_data)
+q_fitting_sensitivity_analysis_df.to_csv(q_fitting_sensitivity_analysis_file)
+
+# Prediction
+q_predictions_sensitivity_analysis_df = pd.DataFrame(q_predictions_sensitivity_analysis_data)
+q_predictions_sensitivity_analysis_df.to_csv(q_predictions_sensitivity_analysis_data)
