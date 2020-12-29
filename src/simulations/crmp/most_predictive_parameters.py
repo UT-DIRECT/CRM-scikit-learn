59568fc20a115b27617b56fc07d061a34cc99edc
Akhil Potla <akhil4400@gmail.com>
2020-12-29 12:19:40 -0600

    Model trained across reservoir

--- src/simulations/crmp/most_predictive_parameters.py
+++ src/simulations/crmp/most_predictive_parameters.py
@@ -12,50 +12,44 @@ from src.helpers.cross_validation import forward_walk_splitter
 from src.helpers.features import production_rate_dataset
 from src.helpers.models import model_namer, test_model
 from src.models.crmp import CRMP
+from src.simulations import number_of_producers, param_grid
 
 
 most_predictive_parameters_file = INPUTS['crmp']['crmp']['predict']['most_predictive_parameters']
 most_predictive_parameters_data = {
-    'tau': [], 'f1': [], 'f2': [], 'q1_mse': [], 'q2_mse': [], 'q3_mse': [],
-    'q4_mse': [], 'aggregate_mses': []
+    'Producer': [], 'tau_initial': [], 'tau_final': [], 'f1_initial': [],
+    'f1_final': [], 'f2_initial': [], 'f2_final': [], 'r2': [], 'MSE': []
 }
 
-f1 = np.linspace(0, 1, 6)
-f2 = np.ones(6) - f1
-tau = np.linspace(1e-6, 100, 10)
-# TODO: I might be able to construct this using a meshgrid
-param_grid = {'p0': []}
-for i in tau:
-    for j in range(len(f1)):
-        param_grid['p0'].append([i, f1[j], f2[j]])
-
-
-def predictive_value_of_different_parameters():
-    X, y = production_rate_dataset(producers[0], *injectors)
-    crmp = CRMP().fit(X, y)
+def train_crmp_across_wells():
+    characteristic_producer = np.mean(producers, axis=0)
+    X, y = production_rate_dataset(characteristic_producer, *injectors)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, shuffle=False
+    )
     for p0 in param_grid['p0']:
-        tau = p0[0]
-        gains = [p0[1], p0[2]]
-        crmp.tau_ = tau
-        crmp.gains_ = gains
-        mses = 0
-
-        for i in range(len(producers)):
-            X, y = production_rate_dataset(producers[i], *injectors)
-            X_train, X_test, y_train, y_test = train_test_split(
-                X, y, test_size=0.2, shuffle=False
+        crmp = CRMP(p0=p0)
+        crmp = crmp.fit(X_train, y_train)
+        mse = 0
+        r2 = 0
+        for i in range(number_of_producers):
+            X_i, y_i = production_rate_dataset(producers[i], *injectors)
+            X_train_i, X_test_i, y_train_i, y_test_i = train_test_split(
+                X_i, y_i, test_size=0.2, shuffle=False
             )
-            y_hat = crmp.predict(X_test)
-            r2, mse = fit_statistics(y_hat, y_test)
-            most_predictive_parameters_data['q{}_mse'.format(i + 1)].append(mse)
-            mses += mse
+            y_hat = crmp.predict(X_test_i)
+            r2_i, mse_i = fit_statistics(y_hat, y_test_i)
+            most_predictive_parameters_data['Producer'].append(i + 1)
+            most_predictive_parameters_data['tau_initial'].append(p0[0])
+            most_predictive_parameters_data['tau_final'].append(crmp.tau_)
+            most_predictive_parameters_data['f1_initial'].append(p0[1])
+            most_predictive_parameters_data['f1_final'].append(crmp.gains_[0])
+            most_predictive_parameters_data['f2_initial'].append(p0[2])
+            most_predictive_parameters_data['f2_final'].append(crmp.gains_[1])
+            most_predictive_parameters_data['r2'].append(r2_i)
+            most_predictive_parameters_data['MSE'].append(mse_i)
 
-        most_predictive_parameters_data['tau'].append(tau)
-        most_predictive_parameters_data['f1'].append(gains[0])
-        most_predictive_parameters_data['f2'].append(gains[1])
-        most_predictive_parameters_data['aggregate_mses'].append(mses)
+    most_predictive_parameters_df = pd.DataFrame(most_predictive_parameters_data)
+    most_predictive_parameters_df.to_csv(most_predictive_parameters_file)
 
-
-predictive_value_of_different_parameters()
-most_predictive_parameters_df = pd.DataFrame(most_predictive_parameters_data)
-most_predictive_parameters_df.to_csv(most_predictive_parameters_file)
+train_crmp_across_wells()
