917869385d86ab45e0f5da7c5194f3a10af78fbd
Akhil Potla <akhil4400@gmail.com>
2022-05-31 15:03:17 -0500

    Adding try except to avoid errors caused by large values.

--- src/helpers/analysis.py
+++ src/helpers/analysis.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 
 from sklearn.metrics import mean_squared_error, r2_score
@@ -7,13 +8,19 @@ from sklearn.utils.validation import check_consistent_length, check_array
 def fit_statistics(y_hat, y, shutin=False):
     if shutin:
         y_hat, y = remove_shutins(y_hat, y)
-    if len(y_hat) == 0:
-        return (0, 0)
-    elif len(y_hat) < 2:
-        r2 = np.nan
-    else:
-        r2 = r2_score(y_hat, y)
-    mse = mean_squared_error(y_hat, y)
+    try:
+        if len(y_hat) == 0:
+            return (0, 0)
+        elif len(y_hat) < 2:
+            r2 = np.nan
+        else:
+            r2 = r2_score(y_hat, y)
+    except:
+        r2 = 0
+    try:
+        mse = mean_squared_error(y_hat, y)
+    except:
+        mse = sys.float_info.max
     return (r2, mse)
 
 
