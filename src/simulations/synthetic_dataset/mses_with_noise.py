d0b3c2afed561d63d50064577cf04209e04db62c
Akhil Potla <akhil4400@gmail.com>
2022-06-03 15:40:25 -0500

    Removing unused code part 3

--- src/simulations/synthetic_dataset/mses_with_noise.py
+++ src/simulations/synthetic_dataset/mses_with_noise.py
@@ -20,7 +20,6 @@ from src.helpers.features import production_rate_dataset
 from src.simulations import injector_names, number_of_producers
 
 
-stds = [1, 10, 25]
 estimators = [CRMP()]
 estimators = [
     LinearRegression(), BayesianRidge(), HuberRegressor(), MLPRegressor()
@@ -31,30 +30,29 @@ def log_transformation(column):
     return np.log(column + 1)
 
 
-for std in stds:
-    print('Standard Deviation: ', std)
-    for estimator in estimators:
-        print(estimator)
-        for i in range(number_of_producers):
-            producer = producers[i]
-            producer += np.random.normal(loc=0.0, scale=std, size=len(producer))
-            X, y = production_rate_dataset(producer, *injectors)
-            X_train, X_test, y_train, y_test = train_test_split(
-                X, y, train_size=0.5, shuffle=False
-            )
-            model = estimator.fit(X_train, y_train)
-            y_hat = []
-            # y_hat = model.predict(X_test[:30, 1:])
-            y_hat_i = y_train[-1]
-            for i in range(30):
-                X_test_i = X_test[i, :]
-                X_test_i[0] = y_hat_i
-                X_test_i = X_test_i.reshape(1, -1)
-                y_hat_i = model.predict(X_test_i)
-                y_hat.append(y_hat_i)
-            r2, mse = fit_statistics(y_hat, y_test[:30])
-            print(mse)
-            print(min(y_hat))
-        print()
+std = 25
+for estimator in estimators:
+    print(estimator)
+    for i in range(number_of_producers):
+        producer = producers[i]
+        producer += np.random.normal(loc=0.0, scale=std, size=len(producer))
+        X, y = production_rate_dataset(producer, *injectors)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, train_size=0.5, shuffle=False
+        )
+        model = estimator.fit(X_train, y_train)
+        y_hat = []
+        # y_hat = model.predict(X_test[:30, 1:])
+        y_hat_i = y_train[-1]
+        for i in range(30):
+            X_test_i = X_test[i, :]
+            X_test_i[0] = y_hat_i
+            X_test_i = X_test_i.reshape(1, -1)
+            y_hat_i = model.predict(X_test_i)
+            y_hat.append(y_hat_i)
+        r2, mse = fit_statistics(y_hat, y_test[:30])
+        print(mse)
+        print(min(y_hat))
     print()
-    print()
+print()
+print()
